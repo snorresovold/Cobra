d6b1e7cdb69f957a4fb5a9c1c419727967024c77
Snorre Søvold <73354016+snorresovold@users.noreply.github.com>
2022-11-21 13:11:15 +0100

    start

---
+++ lexer/tokens.go
@@ -0,0 +1,42 @@
+package lexer
+
+type Token int
+
+// different types
+const (
+	EOF = iota
+	ILLEGAL
+	IDENT
+	INT
+	SEMI // ;
+
+	// Infix ops
+	ADD // +
+	SUB // -
+	MUL // *
+	DIV // /
+
+	ASSIGN // =
+)
+
+// list of tokens we accept
+var tokens = []string{
+	EOF:     "EOF",
+	ILLEGAL: "ILLEGAL",
+	IDENT:   "IDENT",
+	INT:     "INT",
+	SEMI:    ";",
+
+	// Infix ops
+	ADD: "+",
+	SUB: "-",
+	MUL: "*",
+	DIV: "/",
+
+	ASSIGN: "=",
+}
+
+// returns the type of the token from the array of tokens we have
+func (t Token) String() string {
+	return tokens[t]
+}
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/snorresovold/cobra/lexer"
+)
+
+const (
+	EOF = iota
+	ILLEGAL
+	IDENT
+	INT
+	SEMI // ;
+
+	// Infix ops
+	ADD // +
+	SUB // -
+	MUL // *
+	DIV // /
+
+	ASSIGN // =
+)
+
+func main() {
+	file, err := os.Open("input.test")
+	if err != nil {
+		panic(err)
+	}
+
+	lexer := lexer.NewLexer(file)
+
+	for {
+		pos, tok, lit := lexer.Lex()
+		if tok == EOF {
+			break
+		}
+
+		fmt.Printf(lit, pos)
+	}
+}
