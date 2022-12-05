e873500e861b5d86d0b7f9af914a6009df2fbcb8
snorresovold <snorresovold@gmail.com>
2022-12-05 19:56:20 +0100

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,7 +1,29 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
-serve((_req) => {
-  return new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
-  });
-});
+const html = `
+<form method="POST" action="/">
+  <input type="text" name="name" placeholder="Your name">
+  <button type="submit">Submit</button>
+</form>
+`;
+
+async function handler(req: Request): Promise<Response> {
+  switch (req.method) {
+    case "GET": {
+      return new Response(html, {
+        headers: { "content-type": "text/html; charset=utf-8" },
+      });
+    }
+
+    case "POST": {
+      const body = await req.formData();
+      const name = body.get("name") || "anonymous";
+      return new Response(`Hello ${name}!`);
+    }
+
+    default:
+      return new Response("Invalid method", { status: 405 });
+  }
+}
+
+serve(handler);
