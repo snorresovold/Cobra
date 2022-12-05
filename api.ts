506a117529201ab35dba83bffc8a404e5f231675
snorresovold <snorresovold@gmail.com>
2022-12-05 20:08:51 +0100

    Update api.ts

--- api.ts
+++ api.ts
@@ -27,7 +27,7 @@ async function handler(req: Request): Promise<Response> {
         const name = body.get("name") || "anonymous";
         const program = parser.produceAST(name?.toString());
         const result = evaluate(program, env);
-        return new Response(`${result.type.valueOf()}`);
+        return new Response(`${result.type.toString()}`);
         }
 
         default:
