070f354ea10139c7b5b33bbc1f6712315dfab5c9
snorresovold <snorresovold@gmail.com>
2022-12-06 09:32:22 +0100

    react

--- api.ts
+++ api.ts
@@ -1,35 +1,19 @@
-
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
-import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
 
 import Parser from "./frontend/parser.ts";
 import Environment from "./runtime/environment.ts";
 import { evaluate } from "./runtime/interpreter.ts";
 
-function App() {
-    return (
-      <html>
-        <head>
-          <title>Hello from JSX</title>
-        </head>
-        <body>
-          <h1>Hello world</h1>
-          <form method="POST" action="/">
-            <input type="text" name="name" placeholder="Your name">
-            <input type="text" name="name" placeholder="Do some math">
-            <button type="submit">Submit</button>
-        </form>
-        </body>
-      </html>
-    );
-  }
-  
+const html = `
+<form method="POST" action="/">
+  <input type="text" name="name" placeholder="Do some math">
+  <button type="submit">Submit</button>
+</form>
+`;
 
 async function handler(req: Request): Promise<Response> {
     const parser = new Parser();
     const env = new Environment();
-
-    const html = renderSSR(<App />)
     
     switch (req.method) {
         case "GET": {
