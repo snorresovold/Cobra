e1b070c1cb8b6d38cd882237efd107f03a790345
Snorre Søvold <73354016+snorresovold@users.noreply.github.com>
2022-12-06 09:41:47 +0100

    Update test.jsx

--- test.jsx
+++ test.jsx
@@ -7,6 +7,10 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.20/mod.ts";
 
+import Parser from "./frontend/parser.ts";
+import Environment from "./runtime/environment.ts";
+import { evaluate } from "./runtime/interpreter.ts";
+
 function App() {
   return (
     <html>
@@ -24,13 +28,26 @@ function App() {
   );
 }
 
-function handler(req) {
+async function handler(req) {
+  const parser = new Parser();
+  const env = new Environment();
+     
+
   const html = renderSSR(<App />);
-  return new Response(html, {
-    headers: {
-      "content-type": "text/html",
-    },
-  });
+  switch (req.method) {
+    case "GET": {
+      return new Response(html, {
+        headers: {"content-type": "text/html; charset=utf-8"}
+      })
+    }
+    case "POST": {
+      const body = await req.formData();
+      const name = body.get("name") || "anonymous";
+      const program = parser.produceAST(name?.toString());
+      const result = evaluate(program, env);
+      return new Response(JSON.stringify(result))
+    }
+  }
 }
 
 serve(handler);
