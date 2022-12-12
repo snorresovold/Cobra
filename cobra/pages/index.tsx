1f5b94275e7d9fbfb0b08b4442c6620c946eff5e
snorresovold <snorresovold@gmail.com>
2022-12-12 10:29:46 +0100

    Update index.tsx

--- cobra/pages/index.tsx
+++ cobra/pages/index.tsx
@@ -4,8 +4,12 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-     
+    <div>
+      <h1>hello world</h1>
+      <form method="POST" action="/">
+      <input type="text" name="name" placeholder="Do some math">
+      <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
